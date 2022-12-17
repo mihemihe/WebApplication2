7038bc0f2181041a8eb268f0bdf33a3d2ff9c49e
Eduardo Martin <mihemihe@hotmail.com>
2022-12-17 13:31:54 +0400

    Add project files.

---
+++ WebApplication2/BackgroundTask.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+namespace WebApplication2
+{
+    public class BackgroundTask : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Task.Run(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("\r\n---Before writing to file");
+                    
+                    WriteFile.WriteToFile();
+                    
+                    Console.WriteLine("\r\n---After writing to file");
+
+
+                    int seconds = 5;
+                    Console.WriteLine("\r\n--------- END ------------------ | Waiting {0} seconds", seconds);
+                    await Task.Delay(new TimeSpan(0, 0, seconds)); // 5 second delay
+                }
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebApplication2/WriteFile.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication2
+{
+    public static class WriteFile
+    {
+        // add a new method to write a file using streamwriter and using
+        public static void WriteToFile()
+        {
+            string filePath = "c:\\folderdontexist\\thisfiledoesnotexist.txt";
+            using (StreamWriter sw = File.AppendText(filePath)) { sw.WriteLine("this is a literal string to write on the file"); }
+        }
+
+        
+
+
+    }
+}
